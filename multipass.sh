5da4f8700bfd5333c61f0b45521ddda8d1eeed17
Ubuntu <gabe.villalon@gmail.com>
2022-03-27 11:09:08 -0300

    testing bash scripts wip

---
+++ multipass.sh
@@ -0,0 +1,8 @@
+read -p 'insert number of CPU cores: ' core_number
+read -p 'insert allocated disk space (ie 10G): ' disk_space
+read -p 'insert allocated ram memory (ie 2G): ' ram_memory 
+read -p 'insert multipass virtual disk name: ' disk_name
+
+multipass launch -v -c $core_number -d $disk_space -m $ram_memory -n $disk_name
+ 
+multipass shell $disk_name
