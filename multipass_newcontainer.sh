f45e3c1d603830b9d1d515368c9e9b2fc35ca830
Gabriel Villalon <gabe.villalon@gmail.com>
2022-03-27 19:46:11 -0300

    wip

--- multipass_newcontainer.sh
+++ multipass_newcontainer.sh
@@ -4,5 +4,5 @@ read -p 'insert allocated ram memory (ie 2G): ' ram_memory
 read -p 'insert multipass virtual disk name: ' disk_name
 
 multipass launch -v -c $core_number -d $disk_space -m $ram_memory -n $disk_name
- 
+
 multipass shell $disk_name
